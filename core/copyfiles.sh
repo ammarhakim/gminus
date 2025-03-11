a39b3596bb42a491fa8aafbca712495dda589357
ammarhakim <a.hakim777@gmail.com>
2025-03-11 12:04:57 -0400

    Second attempt at dir reorg.

---
+++ core/copyfiles.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+CP_CMD=cp
+G0=/home/gandalf/research/gkyl-project/g0-branches/dir-reorg-fixes/gkylzero
+
+# data
+mkdir -p data/unit
+cp $G0/data/unit/euler_riem_2d_hllc-euler_1.gkyl data/unit/
+cp $G0/data/unit/ser-euler_riem_2d_hllc-euler_1.gkyl data/unit/
+
+# kernels
+
+# minue
+
+# unit
+
+# zero
